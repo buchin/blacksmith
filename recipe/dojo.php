3dc553450ae307b7115ac6ea097506af52f11e5b
Mochammad Masbuchin <buchin@dropsugar.com>
2024-09-09 23:06:20 +0700

    Feature: dojo:ssh_key task

--- recipe/dojo.php
+++ recipe/dojo.php
@@ -8,13 +8,6 @@ task('dojo:provision', [
 ]);
 
 task('dojo:provision:setup', function (){
-    writeln('
-   __   __         __              _ __  __ 
-  / /  / /__ _____/ /__ ___ __ _  (_) /_/ / 
- / _ \/ / _ `/ __/  \'_/(_-</  \' \/ / __/ _ \
-/_.__/_/\_,_/\__/_/\_\/___/_/_/_/_/\__/_//_/
-==== https://dojo.cc/blacksmith-deployer ====
-    ');
     writeln('Starting up');
     set('remote_user', 'root');
 });
@@ -24,5 +17,13 @@ task('dojo:provision:done', function (){
     set('remote_user', 'deployer');
     $ssh_key = run("cat ~/.ssh/id_rsa.pub");
 
-    info('If this is your first time provisioning this server, add this ssh key to your github/bitbucket account: ' . $ssh_key);
+    info('If this is your first time provisioning this server, you should add ssh key to your github/bitbucket account. Run: php blacksmith dojo:ssh_key');
+});
+
+task('dojo:ssh_key', function (){
+    info('Getting SSH Key.');
+    set('remote_user', 'deployer');
+    $ssh_key = run("cat ~/.ssh/id_rsa.pub");
+
+    echo "<fg=black;bg=bright-green>$ssh_key</>";
 });
